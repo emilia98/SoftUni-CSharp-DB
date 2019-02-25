3c3dbd84f9834711a9f94d699fb54eef44351994
unknown <emilia_nedialkova@abv.bg>
2019-02-26 00:23:43 +0200

    added part of homework

---
+++ DB Advanced - Entity Framework/02. Fetching Resultsets With ADO.NET/MinionsDB/01.InitialSetup/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace _01.InitialSetup
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-1DUHAQR\SQLEXPRESS;;Integrated Security=True";
+    }
+}
