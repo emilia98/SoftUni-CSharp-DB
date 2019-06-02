c569f731d5d397d29f083473db1ea17ad04fe1f6
emilia98 <emilia_nedialkova@abv.bg>
2019-06-02 23:08:57 +0300

    init 2 new exams

---
+++ DB-Basics-MSSQL-Server/Exams/Databases Exam - 12 July 2015/01.All Diablo Characters.sql
@@ -0,0 +1,25 @@
+USE "Diablo";
+
+
+SELECT Name
+FROM "Characters"
+ORDER BY Name ASC;
+
+/* 
+OUTPUT: 
+Name
+Amazon
+Assassin
+Barbarian
+Crusader
+Demon Hunter
+Druid
+Monk
+Necromancer
+Paladin
+Sorceress
+Witch Doctor
+Wizard
+
+(12 rows affected)
+*/
---
+++ DB-Basics-MSSQL-Server/Exams/Databases Sample Exam/01.All Ad Titles.sql
@@ -0,0 +1,34 @@
+USE "Ads";
+
+SELECT
+	Title
+FROM 
+	"Ads"
+ORDER BY 
+	Title ASC;
+
+/*
+OUTPUT: 
+---------------------------------------------
+Title
+2 bed apartment to let
+2012 Nissan Altima 3.5 SR 2dr Coupe CVT
+3 bed apartment to let
+Afterschool Bus Driver
+BMW 320 for Sale
+Ford Mondeo Zetec 1.8TDCI, 2009
+Free cat
+Free cat. Great hunter
+Free dog
+Free MP3 Player
+Free porno
+iPad 4 128GB 4G
+iPhone 4S for Sale
+Job as Housekeeper
+Just a Joke
+Lexus SC 430 Gold (2006)
+Nikon D5100 DSLR c/w 18-55mm Lens + Kit
+Pharmacy Technician Job (Walgreens)
+Skandika Vancouver 6 Tent
+------------------------------------------
+*/
